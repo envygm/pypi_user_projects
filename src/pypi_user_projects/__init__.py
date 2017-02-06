0cb135b750375ca5d1b74645a87ad2ce9f89de9e
envygm <envygm@gmail.com>
2017-02-07 00:27:31 +0100

    added get_html and parse_projects

--- src/pypi_user_projects/__init__.py
+++ src/pypi_user_projects/__init__.py
@@ -1,1 +1,23 @@
 #! /user/bin/env python3
+
+import requests
+import dhtmlparser
+
+def get_html(nick):
+    url = "https://pypi.python.org/pypi?%3Aaction=search&term=" + nick + "&submit=search"
+
+    response = requests.get(url)
+
+    return response.text
+
+#print(get_html("bystroushaak"))
+
+def parse_projects(html):
+    dom = dhtmlparser.parseString(html)
+    table = dom.find("table", {"class": "list"} )
+
+    return table
+
+print(parse_projects(get_html("bystroushaak")))
+
+
